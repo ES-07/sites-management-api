685bd684faa5f95141059041421eae08753cec9a
artur-romao <arturomao@gmail.com>
2022-11-27 20:07:31 +0000

    fixed config i guess

--- app/core/config.py
+++ app/core/config.py
@@ -6,24 +6,10 @@ import os
 PROJECT_NAME='cctv'
 VERSION='1.0.0'
 
-config = Config(".env")
-
-SECRET_KEY = config("SECRET_KEY", cast=Secret, default="CHANGEME")
-
-""" POSTGRES_USER = config(os.environ['POSTGRES_USER'], cast=str, default="postgres")
-POSTGRES_PASSWORD = config(os.environ['POSTGRES_PASSWORD'], cast=Secret, default="postgres")
-POSTGRES_SERVER = config(os.environ['POSTGRES_SERVER'], cast=str, default="postgres")
+POSTGRES_USER = os.environ['POSTGRES_USER']
+POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
+POSTGRES_SERVER = os.environ['POSTGRES_SERVER']
 POSTGRES_PORT = "5433"
-POSTGRES_DB = config(os.environ['POSTGRES_DB'], cast=str, default="postgres") """
+POSTGRES_DB = os.environ['POSTGRES_DB']
 
-POSTGRES_USER = config("postgres", cast=str, default="postgres")
-POSTGRES_PASSWORD = config("postgres", cast=Secret, default="postgres")
-POSTGRES_SERVER = config("15.237.96.214", cast=str, default="postgres")
-POSTGRES_PORT = "5433"
-POSTGRES_DB = config("postgres", cast=str, default="postgres")
-
-DATABASE_URL = config(
-  "DATABASE_URL",
-  cast=str,
-  default=f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
-)
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
