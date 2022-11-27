8fcacb3310f46f93d0cd160ac0e78d38ab81f460
artur-romao <arturomao@gmail.com>
2022-11-27 19:59:38 +0000

    checking without secrets

--- app/core/config.py
+++ app/core/config.py
@@ -10,11 +10,17 @@ config = Config(".env")
 
 SECRET_KEY = config("SECRET_KEY", cast=Secret, default="CHANGEME")
 
-POSTGRES_USER = config(os.environ['POSTGRES_USER'], cast=str, default="postgres")
+""" POSTGRES_USER = config(os.environ['POSTGRES_USER'], cast=str, default="postgres")
 POSTGRES_PASSWORD = config(os.environ['POSTGRES_PASSWORD'], cast=Secret, default="postgres")
 POSTGRES_SERVER = config(os.environ['POSTGRES_SERVER'], cast=str, default="postgres")
 POSTGRES_PORT = "5433"
-POSTGRES_DB = config(os.environ['POSTGRES_DB'], cast=str, default="postgres")
+POSTGRES_DB = config(os.environ['POSTGRES_DB'], cast=str, default="postgres") """
+
+POSTGRES_USER = config("postgres", cast=str, default="postgres")
+POSTGRES_PASSWORD = config("postgres", cast=Secret, default="postgres")
+POSTGRES_SERVER = config("15.237.96.214", cast=str, default="postgres")
+POSTGRES_PORT = "5433"
+POSTGRES_DB = config("postgres", cast=str, default="postgres")
 
 DATABASE_URL = config(
   "DATABASE_URL",
