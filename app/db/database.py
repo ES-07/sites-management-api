c899816767fc0e9550d5bd82a3b9d6bb8f1f9e4d
artur-romao <arturomao@gmail.com>
2022-11-27 20:21:24 +0000

    check why url isnt working

--- app/db/database.py
+++ app/db/database.py
@@ -3,8 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-raise Exception(DATABASE_URL)
-
 SQLALCHEMY_DATABASE_URL = DATABASE_URL
 
 engine = create_engine(
